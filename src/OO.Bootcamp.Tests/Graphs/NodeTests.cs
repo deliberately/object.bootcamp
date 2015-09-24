9b4abe2b9edb98eace151d0e1f649d6eb36d9648
Tom Scott <tomwscott@gmail.com>
2015-09-24 16:44:31 +0100

    Adds HopCount behaviour

--- src/OO.Bootcamp.Tests/Graphs/NodeTests.cs
+++ src/OO.Bootcamp.Tests/Graphs/NodeTests.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using NUnit.Framework;
 
@@ -7,19 +8,20 @@ namespace OO.Bootcamp.Tests.Graphs
     [TestFixture]
     public class NodeTests
     {
-        private Node a = new Node("a");
-        private Node b = new Node("b");
-        private Node c = new Node("c");
-        private Node d = new Node("d");
-        private Node e = new Node("e");
-        private Node f = new Node("f");
-        private Node g = new Node("g");
+        private Node a = new Node();
+        private Node b = new Node();
+        private Node c = new Node();
+        private Node d = new Node();
+        private Node e = new Node();
+        private Node f = new Node();
+        private Node g = new Node();
 
         [SetUp]
         public void Setup()
         {
             b.LinkTo(a);
             b.LinkTo(c).LinkTo(d).LinkTo(e).LinkTo(b).LinkTo(f);
+            c.LinkTo(e);
             c.LinkTo(d);
         }
 
@@ -36,27 +38,54 @@ namespace OO.Bootcamp.Tests.Graphs
             Assert.That(c.CanReach(d));
             Assert.That(c.CanReach(f));
         }
+
+        [Test]
+        public void ShouldDetermineHopCount()
+        {
+            Assert.That(b.HopsTo(b), Is.EqualTo(0));
+            Assert.That(b.HopsTo(a), Is.EqualTo(1));
+            Assert.That(c.HopsTo(e), Is.EqualTo(1));
+            Assert.That(c.HopsTo(f), Is.EqualTo(3));
+        }
+
+        [Test]
+        public void ShouldFailWhenAttemptingToReachAnUnreachableNode()
+        {
+            Assert.Throws<NoPathExistsException>(() => a.HopsTo(b));
+        }
     }
 
+    public class NoPathExistsException : Exception
+    {
+        
+    }
     public class Node
     {
         private readonly List<Node> neighbours = new List<Node>();
-
-        public Node(string name)
-        {
-            
-        }
+        private const double NotFound = double.PositiveInfinity;
 
         public bool CanReach(Node destination)
         {
-            return CanReach(destination, new List<Node>());
+            return HopsTo(destination, new List<Node>()) != NotFound;
         }
 
-        private bool CanReach(Node destination, List<Node> visitedNodes)
+        public int HopsTo(Node destination)
         {
-            if (this.Equals(destination)) return true;
-            if (visitedNodes.Contains((this))) return false;
-            return neighbours.Any(n => n.CanReach(destination, visitedNodes.With(this)));
+            var result = HopsTo(destination, new List<Node>());
+            if (result.Equals(NotFound))
+            {
+                throw new NoPathExistsException();
+            }
+            return Convert.ToInt32(result);
+        }
+
+        private double HopsTo(Node destination, List<Node> visitedNodes)
+        {
+            if (Equals(destination)) return 0;
+            if (visitedNodes.Contains((this))) return NotFound;
+            visitedNodes.Add(this);
+            var validHops = neighbours.Select(n => n.HopsTo(destination, visitedNodes.Copy()) + 1).ToList();
+            return validHops.Any() ? validHops.Min() : NotFound;
         }
 
         public Node LinkTo(Node neighbour)
@@ -72,6 +101,11 @@ namespace OO.Bootcamp.Tests.Graphs
         {
             list.Add(item);
             return list;
+        }
+
+        public static List<T> Copy<T>(this List<T> list)
+        {
+            return new List<T>(list);
         }
     }
 }
